03eb70cf58c05831bccf7bd9c8fae696402f6d88
Edwin Kato <katedwin88@gmail.com>
2017-04-26 21:50:54 +0300

    Change create_room to cater for living and office space

--- src/dojo.py
+++ src/dojo.py
@@ -10,10 +10,13 @@ class Dojo(object):
     def create_room(self, room_type, *room_names):
         created_rooms = []
         for room_name in room_names:
-            room = Room(room_type, room_name)
-            self.all_rooms.append(room)
-            created_rooms.append(room)
-            print("An {0} called {1} has been successfully created!".format(room_type, room_name))
+            if room_name not in [room.room_name for room in self.all_rooms]:
+                room = Office(room_type, room_name) if room_type == "office" else LivingSpace(room_type, room_name)
+                self.all_rooms.append(room)
+                created_rooms.append(room)
+                print("An {0} called {1} has been successfully created!".format(room_type, room_name))
+            else:
+                print("Room with name: {0} already exists. Please try using another name".format(room_name))
         return created_rooms[0] if len(created_rooms) == 1 else created_rooms
 
 
