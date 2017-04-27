51084b951d7d878d5400a6bfca1c8da1793b0a17
Edwin Kato <katedwin88@gmail.com>
2017-04-27 14:39:55 +0300

    Modify init method of staffto take in staff person type only

--- src/staff.py
+++ src/staff.py
@@ -3,6 +3,6 @@ from .person import Person
 
 class Staff(Person):
 
-    def __init__(self, first_name, last_name, person_type, person_id, has_living_space = None, has_office = None):
-        super(Staff, self).__init__(first_name, last_name, person_type, "N", person_id, has_living_space, has_office)
+    def __init__(self, first_name, last_name, person_id, has_living_space = None, has_office = None):
+        super(Staff, self).__init__(first_name, last_name, "staff", "N", person_id, has_living_space, has_office)
 
